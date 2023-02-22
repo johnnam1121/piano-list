b7ab67ab158a847af470d0838572868dcd9e1112
johnnam1121 <johnnam93@gmail.com>
2023-02-22 17:10:19 -0600

    first commit

---
+++ src/components/Video.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import ReactPlayer from 'react-player'
+
+function Video(props) {
+  return (
+    <Container fluid>
+      <Row className='align-items-center mt-5 mb-5' >
+        <Col className='shadow p-3 mb-5 rounded' sm={{ span: 4, offset: 2 }} style={{ backgroundColor: 'black' }}>
+          <ReactPlayer
+            controls
+            url={props.video}
+            // height="75%"
+            playing={false}
+            width="100%"
+          />
+        </Col>
+        <Col className='shadow p-3 mb-5 rounded' sm={{ offset: 1, span: 4 }}>
+          <h1>{props.title}</h1>
+          <p>Sheets here<a href={props.google + props.title + " piano sheet music"} target="_blank" rel="noreferrer" >{props.title}</a></p>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Video
---
+++ src/components/YoutubeFetch.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import Video from './Video';
+
+const API = 'AIzaSyBjz1KrnlXtaREQKbCmTwv-smz5_KpxTTg';
+const playlistID = 'PLfWl6eNXF37Kh5hEr2gIKxjJTDSw7iIeb';
+const result = 35;
+
+var url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=${result}&playlistId=${playlistID}&key=${API}`
+
+export class YoutubeFetch extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      playlist: [],
+      title: [],
+      description: [],
+      thumbnails: [],
+    };
+  }
+
+  componentDidMount() {
+    return fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        // console.log('responseJson', responseJson)
+        const playlist = responseJson.items.map(obj => "https://www.youtube.com/embed/" + obj.snippet.resourceId.videoId);
+        this.setState({ playlist });
+        const description = responseJson.items.map(obj => obj.snippet.description);
+        this.setState({ description });
+        const thumbnails = responseJson.items.map(obj => obj.snippet.thumbnails.default.url);
+        this.setState({ thumbnails });
+        const title = responseJson.items.map(obj => obj.snippet.title);
+        this.setState({ title });
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          {this.state.playlist.map((value, index) =>
+            <Video
+              key={index}
+              video={this.state.playlist[index]}
+              google={"http://www.google.com/search?q="}
+              title={this.state.title[index]}
+              description={this.state.description[index]}
+            />
+          )}
+        </Row>
+      </Container>
+    )
+  }
+}
+
+export default YoutubeFetch
